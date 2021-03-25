4ff24c8b84707fbb5a7cd7a3f78acfcd2fe65c27
justinjarrard <jjarrard21@gmail.com>
2021-03-24 20:02:05 -0400

    fixed sequelize problems

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,18 @@
+// require('dotenv').config()
+
+// const Sequelize = require('sequelize')
+
+// const sequelize = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
+
+// module.exports = sequelize
+
 require('dotenv').config()
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
+// const sequelize = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
+
+const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL) : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { host: "localhost", dialect: "mysql", dialectOptions: { decimalNumbers: true } })
 
 module.exports = sequelize
+
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-require('dotenv').config()
+// require('dotenv').config()
+
+// const express = require('express')
+// const sequelize = require('./models/Product')
+// // import sequelize connection
+
+// const app = express()
+
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+
+// app.use(require('./routes'))
+
+// // sync sequelize models to the database, then turn on the server
+// require('./config/connection.js').sync()
+//   .then(() => app.listen(process.env.PORT || 3000))
+//   .catch(err => console.log(err))
 
 const express = require('express')
-const sequelize = require('./models/Product')
 // import sequelize connection
+const sequelize = require('./config/connection.js')
 
 const app = express()
+
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -12,6 +30,5 @@ app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes'))
 
 // sync sequelize models to the database, then turn on the server
-require('./config/connect.js').sync()
-  .then(() => app.listen(process.env.PORT || 3000))
-  .catch(err => console.log(err))
+sequelize.sync({ force: false })
+  .then(() => { app.listen(PORT), () => { console.log('app is live on port 3000') } })
